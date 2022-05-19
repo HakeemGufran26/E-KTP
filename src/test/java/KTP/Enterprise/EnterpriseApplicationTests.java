741eb5e53b46b914e98578c61192e93e78192b2d
Hakeem Gufran <hakeemguran26@gmail.com>
2022-05-20 01:11:27 +0700

    first commit

---
+++ src/test/java/KTP/Enterprise/EnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package KTP.Enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
